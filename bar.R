055257de8102532574c75c822cc405871b33a860
erikazanetti <erikazanetti.25@gmail.com>
2025-02-02 18:27:47 +0100

    Update bar.R

--- bar.R
+++ bar.R
@@ -5,3 +5,4 @@ data <- data.frame(classes, year_2017, year_2023)
 
 g1 <- ggplot(data, aes(x=classes, y=year_2017, fill=classes)) + geom_bar(stat="identity") + scale_fill_viridis_d(option = "D") + ylim(c(0, 100)) + labs(title = "Forest Classification in 2017", y = "Percentage", x = "Classes")
 g2 <- ggplot(data, aes(x=classes, y=year_2023, fill=classes)) + geom_bar(stat="identity") +  scale_fill_viridis_d(option = "D") + ylim(c(0, 100)) + labs(title = "Forest Classification in 2023", y = "Percentage", x = "Classes")
+g1 + g2
