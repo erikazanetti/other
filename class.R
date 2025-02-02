3b327b647a0e9c9cf52fd28e228b0026e556abe0
erikazanetti <erikazanetti.25@gmail.com>
2025-02-02 15:54:00 +0100

    Update class.R

--- class.R
+++ class.R
@@ -1,4 +1,14 @@
-clv <- viridis(100)
-par(mfrow=c(1,2)) 
-plot(bandsr17c, col=clv)
-plot(bandsr23c, col=clv)
+f17 <- freq(bandsr17c)
+tot17 <- ncell(bandsr17c)
+
+f23 <- freq(bandsr23c)
+tot23 <- ncell(bandsr23c)
+
+perc17 = f17*100/tot17
+# cleared forest = 41
+# original forest = 59
+perc23 = f23*100/tot23
+# cleared forest = 56
+# original forest = 44
+
+forest_loss_percentage <- perc17 - perc23
