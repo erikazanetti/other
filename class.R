3e3db8ad5c7b5be273a475d7f489da9606865731
erikazanetti <erikazanetti.25@gmail.com>
2025-02-02 15:19:25 +0100

    Update and rename class17.R to class.R

---
+++ class.R
@@ -0,0 +1,4 @@
+clv <- viridis(100)
+par(mfrow=c(1,2)) 
+plot(bandsr17c, col=clv)
+plot(bandsr23c, col=clv)
--- class17.R
+++
@@ -1,8 +0,0 @@
-r17r <- r17[[1]]                        # Red band
-r17g <- r17[[2]]                        # Green band
-r17b <- r17[[3]]                        # Blue band 
-r17nir <- r17f[[1]]                     # NIR band
-bandsr17 <- c(r17r, r17g, r17b, r17nir) # Uniting all the bands
-
-# Directly classifying the image with all the bands
-bandsr17c <- im.classify(bandsr17,num_clusters=2)
