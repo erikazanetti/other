e3372409a606aa8254707513098c1eb555038eb7
erikazanetti <erikazanetti.25@gmail.com>
2025-02-03 12:01:56 +0100

    Update and rename NDVI.R to difndvi.R

--- NDVI.R
+++
@@ -1,2 +0,0 @@
-ndvi17 = dvi17 / (bandsr17[[4]] + bandsr17[[1]])
-ndvi23 = dvi23 / (bandsr23[[4]] + bandsr23[[1]])
---
+++ difndvi.R
@@ -0,0 +1,4 @@
+difNDVI = ndvi17 - ndvi23
+par(mfrow=c(1,2))
+plot(difNDVI,col=cl)
+plot(difNDVI,col=clv)
