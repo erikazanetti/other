d230161fa8ccc53437e3eede40afb5e5edc0a3fe
erikazanetti <erikazanetti.25@gmail.com>
2025-02-03 13:10:19 +0100

    Update and rename pcaf.R to pcaplot.R

--- pcaf.R
+++
@@ -1,3 +0,0 @@
-par(mfrow=c(1,2))
-plot(pcsd17, col=clv)
-plot(pcsd23, col=clv)
---
+++ pcaplot.R
@@ -0,0 +1,3 @@
+par(mfrow=c(1,2))
+plot(pcsd17, col=clv, main="Spatial variability 2017")
+plot(pcsd23, col=clv, main="Spatial variability 2017")
