156a2a702a2255f9d3e6e005dfe2395178af8ed7
erikazanetti <erikazanetti.25@gmail.com>
2025-02-05 10:40:37 +0100

    Update and rename pcaplot.R to difndvi.R

---
+++ difndvi.R
@@ -0,0 +1,4 @@
+difNDVI = ndvi23 - ndvi17
+par(mfrow=c(1,2))
+plot(difNDVI,col=cl)
+plot(difNDVI,col=clv)
--- pcaplot.R
+++
@@ -1,3 +0,0 @@
-par(mfrow=c(1,2))
-plot(pcsd17, col=clv, main="Spatial variability 2017")
-plot(pcsd23, col=clv, main="Spatial variability 2017")
